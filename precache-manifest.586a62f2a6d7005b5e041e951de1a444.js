9e09b40de6bc7ca9ca442a23571bf92d42055718
Raymond Mawina <mawina.projects@gmail.com>
2022-12-17 01:06:26 +0200

    Updates

---
+++ precache-manifest.586a62f2a6d7005b5e041e951de1a444.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8ad789c5ebc4a67c0a98",
+    "url": "/raymond-mawina-react.github.io/static/css/main.e8429836.chunk.css"
+  },
+  {
+    "revision": "8ad789c5ebc4a67c0a98",
+    "url": "/raymond-mawina-react.github.io/static/js/main.8ad789c5.chunk.js"
+  },
+  {
+    "revision": "6325d5e5f9179d4348d7",
+    "url": "/raymond-mawina-react.github.io/static/js/1.6325d5e5.chunk.js"
+  },
+  {
+    "revision": "1a42bf75a4db2b7757f7",
+    "url": "/raymond-mawina-react.github.io/static/js/runtime~main.1a42bf75.js"
+  },
+  {
+    "revision": "72019900793b2c3e9d65c76d8bcf0dc1",
+    "url": "/raymond-mawina-react.github.io/index.html"
+  }
+];
